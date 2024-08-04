017c6d2f7fe7220886207e59e96d96c944fdd15e
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-04 17:59:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
