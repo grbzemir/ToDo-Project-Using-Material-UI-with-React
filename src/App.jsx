2922c606520e2965263212e473cb855ee9133cb6
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-08-04 18:39:05 +0300

    CreateTask and Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import CreateTask from './CreateTask'
 
 function App() {
+
+  const addTask = (newTask) => {
+    console.log(newTask)
+  }
+
   return (
     <div className="App">
       <Header />
+      <CreateTask onAdd={addTask} />
     </div>
   )
 }
---
+++ src/CreateTask.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+export default function CreateTask(props) {
+    const [task, setTask] = useState({
+        title: '',
+        content: '', // Ensure this matches the 'name' attribute in the textarea
+    });
+
+    function detectChange(event) {
+        const { name, value } = event.target;
+
+        setTask((prevTask) => {
+            return { ...prevTask, [name]: value };
+        });
+    }
+
+    function submitTask(event) {
+        props.onAdd(task);
+        setTask({
+            title: '',
+            content: ''
+        });
+        event.preventDefault();
+    }
+
+    return (
+        <div className='todoDivArea'>
+            <form className='todoDiv'>
+                <input
+                    type="text"
+                    className="form-control todoText mb-3"
+                    name="title"
+                    value={task.title}
+                    onChange={detectChange}
+                    placeholder="Başlık"
+                />
+                <textarea
+                    name="content" // Ensure this matches the 'content' property in the state
+                    className="form-control todoText mb-3"
+                    rows={3}
+                    value={task.content}
+                    onChange={detectChange}
+                    placeholder='İşinizi Yazınız'
+                ></textarea>
+                <button className="btn btn-primary todoButton" onClick={submitTask}>Görevi Ekle</button>
+            </form>
+        </div>
+    );
+}
+
+CreateTask.propTypes = {
+    onAdd: PropTypes.func.isRequired
+};
